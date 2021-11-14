29f2b38e91b2dc55b09ea177d1ab18323e4e9c96
Raquel <33493284+nsoroma@users.noreply.github.com>
2021-11-14 01:39:43 -0500

    PW Gen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,39 @@
+
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numeric = "0123456789";
+const specialChars = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword () {
+
+    // Ask user for desired password length; If the length is between 8 and 128 characters then pwLengthCheck = true
+    var pwLength = window.prompt("Enter Desired Password Length");
+    var pwLengthCheck = ((pwLength.length > 7) && (pwLength.length < 129));
+//    var pwTypeCheck = window.prompt(typeof )
+    
+    var buff = new Uint8Array(pwLength);
+
+    if (pwLengthCheck) {
+      var pwDesiredLength = window.prompt("Enter Desired Password Length");
+
+      window.crypto.getRandomValues(buff);
+      return btoa(String.fromCharCode.apply(null,buff));
+      }
+
+      // If user pressed Cancel, immediately end function
+//      else (!pwLengthCheck) {
+      else { 
+      return;
+      }
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +44,13 @@ function writePassword() {
 
 }
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+console.log("Hello");
